585914a0ef6018d514d1ae09f5866a48527a6965
bidibulle23 <fleurdethe31@gmail.com>
2016-12-04 15:25:47 +0100

    Initialisation du projet MesContacts

---
+++ MesContacts/src/main/java/fr/ddoumeng/mescontacts/Person.java
@@ -0,0 +1,38 @@
+package fr.ddoumeng.mescontacts;
+
+import org.springframework.data.annotation.Id;
+
+public class Person {
+
+	@Id
+	private String id;
+
+	private String firstName;
+	private String lastName;
+	private String phoneNumber;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+}
